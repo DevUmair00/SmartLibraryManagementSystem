de81017ead827c4807e9d8ee1264ffa2b922dfe2
DevUmair00 <rkumair203@gmail.com>
2025-12-18 12:49:29 +0500

    add folders

---
+++ Smart_Library.cs
@@ -0,0 +1,10 @@
+namespace Smart_Library_Management_System
+{
+    public partial class Smart_Library : Form
+    {
+        public Smart_Library()
+        {
+            InitializeComponent();
+        }
+    }
+}
